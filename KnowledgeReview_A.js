3228abf9317b73a7e049d702271c5975b7c6c791
A10A-ROCK-F383-\admin <jsrock@it888.com>
2023-04-11 14:28:12 +0800

    first commit

---
+++ KnowledgeReview_A.js
@@ -0,0 +1,167 @@
+let Exam01 = [
+  {
+    tittle: "1. 请完成以下的深度 - 压力 - 空气体积 - 空气密度的表格:",
+    answer: [
+      "A 捏着鼻子,然后试着从鼻子轻轻吹气.",
+      "B 做吞咽的动作,左右活动下巴.",
+      "C 捏着鼻子,然后用力地长时间从鼻子吹气.",
+    ],
+    reallyAnswer: "A dsadasdasdadasd",
+  },
+  {
+    tittle: "2. 下潜时,我需要平衡压力.以下哪些是平衡耳朵的正确技巧?",
+    answer: [
+      "A 捏着鼻子,然后试着从鼻子轻轻吹气.",
+      "B 做吞咽的动作,左右活动下巴.",
+      "C 捏着鼻子,然后用力地长时间从鼻子吹气.",
+    ],
+    reallyAnswer: [
+      "A 捏着鼻子,然后试着从鼻子轻轻吹气.",
+      "B 做吞咽的动作,左右活动下巴.",
+    ],
+  },
+  {
+    tittle: "3. 在下潜时,我试着要平衡压力,但发现有困难,以下何者正确?",
+    answer: [
+      "A 减慢下潜速率,以手势示意我的潜伴也慢下来,然后更用力地平衡压力.",
+      "B 停止下潜,向潜伴比手势,稍微上升,然后再试一次.",
+      "C 持续下潜,同时不断平衡压力.",
+      "D 向潜伴比手势,上升到水面,然后重新下潜.",
+    ],
+    reallyAnswer: ["B 停止下潜,向潜伴比手势,稍微上升,然后再试一次."],
+  },
+  {
+    tittle: "4. 我应该______平衡压力?",
+    answer: [
+      "A 尽早,经常且在我感到不适之前.",
+      "B 在我开始感到不适时.",
+      "C 只在我觉得疼痛时.",
+    ],
+    reallyAnswer: ["A 尽早,经常且在我感到不适之前."],
+  },
+  {
+    tittle: "5. 我不应该在感冒时潜水,也不该在使用感冒药物后潜水.",
+    answer: ["A 正确.", "B 在错误."],
+    reallyAnswer: ["A 正确."],
+  },
+  {
+    tittle: "6. 水肺潜水最重要的规则是:",
+    answer: [
+      "A 持续呼吸且绝对不要憋气.",
+      "B 永远和潜伴一起潜水.",
+      "C 在水中戴着面镜,即使在水面上也是一样.",
+      "D 每几分钟就检查一次我的压力表.",
+    ],
+    reallyAnswer: ["A 持续呼吸且绝对不要憋气."],
+  },
+  {
+    tittle:
+      "7. 没有遵守水肺潜水最重要的规则可能会导致严重的肺部过度扩张(肺破裂)伤害,进而可能导致瘫痪和死亡.",
+    answer: ["A 正确.", "B 错误."],
+    reallyAnswer: ["A 正确."],
+  },
+  {
+    tittle: "8. 若我在上升时身体的空腔感到不适,正确的做法是:",
+    answer: [
+      "A 持续上升,同时捏住鼻孔吹气.",
+      "B 停下来,稍微下潜,让阻塞的空气有时间自行排出.",
+      "C 下潜至少10公尺 / 33英尺,然后恢复上升.",
+    ],
+    reallyAnswer: [, "B 停下来,稍微下潜,让阻塞的空气有时间自行排出."],
+  },
+  {
+    tittle:
+      "9. 我的潜伴和我计划要潜入18公尺/60英尺的深度,在其他情况不变下,我们应该要预期空气供给时间会比在10公尺/33英尺时______.",
+    answer: ["A 更长.", "B 更短.", "C 一样."],
+    reallyAnswer: ["A 更长."],
+  },
+  {
+    tittle:
+      "10. 在潜水时,我为了跟随我感兴趣的鱼而奋力游泳,但很快就感觉我无法呼吸到足够的空气,此时正确的行为是:",
+    answer: [
+      "A 向潜伴比手势,然后回到水面上.",
+      "B 使用调节器排气钮来增加空气流量.",
+      "C 停下所有动作,然后休息直到恢复深沉,缓慢的呼吸.",
+    ],
+    reallyAnswer: ["C 停下所有动作,然后休息直到恢复深沉,缓慢的呼吸."],
+  },
+  {
+    tittle: "11. 一个在淡水中呈现中性浮力的物体,在海水中会_______?",
+    answer: ["A 沉下去.", "B 浮起来.", "C 中性浮力."],
+    reallyAnswer: ["B 浮起来."],
+  },
+  {
+    tittle: "12. 潜伴制度的三个主要好处是实用性,安全性和乐趣.",
+    answer: ["A 正确.", "B 错误."],
+    reallyAnswer: ["A 正确."],
+  },
+  {
+    tittle: "13. 选择任何水肺潜水装备时的主要考量为_____.",
+    answer: ["A 适合.", "B 品牌.", "C 合身.", "D 舒适."],
+    reallyAnswer: ["A 适合.", "C 合身.", "D 舒适."],
+  },
+  {
+    tittle:
+      "14. 我正在计划下周的潜水并且检查我的装备,我将装备组装好之后,发现我的调节器呼吸起来比印象中困难,此时适当的行为是:",
+    answer: [
+      "A 在使用前让专业人士进行检查和保养.",
+      "B 不管它继续用,但限制我的潜水深度为10公尺 / 33英尺.",
+      "C 用清水冲洗,然后查看问题是否还存在.",
+    ],
+    reallyAnswer: ["A 在使用前让专业人士进行检查和保养."],
+  },
+  {
+    tittle: "15. 你无法在yoke气瓶阀上使用DIN调节器.",
+    answer: ["A 正确.", "B 错误."],
+    reallyAnswer: ["B 错误."],
+  },
+  {
+    tittle:
+      "16. 熟练浮力控制技巧非常重要,因为它能让我控制下潜,上浮或是维持中性浮力,我在潜水时经常需要调整浮力.",
+    answer: ["A 正确.", "B 错误."],
+    reallyAnswer: ["A 正确."],
+  },
+];
+
+let ChooseBoxs = document.querySelector(".ChooseBox");
+let backBtn = document.querySelector(".KnowledgeReview_A_left");
+let nextBtn = document.querySelector(".KnowledgeReview_A_right");
+
+let lis = document.querySelectorAll("li");
+// console.log(lis);
+for (let i = 0; i < lis.length; i++) {
+  lis[i].addEventListener("click", function () {
+    // lis[i].querySelector('li :nth-child(1)').classList.add('ChooseBox_active')
+    // // ChooseBox.classList.add("ChooseBox_active");
+    // // document.querySelector('.CHooseBox').classList.remove('ChooseBox_active')
+    // nextBtn.disabled = false;
+    // backBtn.disabled = false;
+
+    if (lis[i].querySelector("li :nth-child(1)").className === "ChooseBox") {
+      lis[i]
+        .querySelector("li :nth-child(1)")
+        .classList.add("ChooseBox_active");
+      nextBtn.disabled = false;
+      backBtn.disabled = false;
+    } else {
+      lis[i]
+        .querySelector("li :nth-child(1)")
+        .classList.remove("ChooseBox_active");
+    }
+
+    // console.log(i);
+  });
+}
+
+function addNew() {
+  for (let i = 0; i < Exam01.length; i++) {
+    const tiDom = document.createElement("div");
+    tiDom.classList.add("KnowledgeReview_A");
+    const ti = Exam01[i].tittle;
+    tiDom.innerHTML = ti;
+
+    console.log(tiDom);
+  }
+}
+
+addNew();
