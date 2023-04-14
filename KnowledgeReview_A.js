dde793f58599d05b4954f9bbaa22b906071e91e6
rock <notime.hy@gmail.com>
2023-04-14 19:33:22 +0800

    11111

--- KnowledgeReview_A.js
+++ KnowledgeReview_A.js
@@ -161,9 +161,9 @@ function addNew() {
     // const answer = Exam01Data[i].answer;
     // tiDom.innerHTML = answer;
 
-    const boxUp = 1
-    const boxMiddle =1
-    const boxDown = 1
+    const boxUp = 10000
+    const boxMiddle =10000
+    const boxDown = 100000
 
     const backBtn = document.createElement("button");
     const backBtnText = document.createTextNode("上一题");
